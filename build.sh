8afbcb2ac32b8f772c74c0ea48f80745582c80ad
Kaplinger <tk185141@wustk185141-ooe.lan>
2024-01-08 16:15:07 -0500

    Update build process to reflect new directory convention

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ docker build --no-cache=true -t todkap/rocky:1.0.0 .
 
 docker push todkap/rocky:1.0.0
 
-helm package -d helm/docs --version 1.0.0 helm/rocky
+helm package -d charts/docs --version 1.0.0 charts/rocky
 
-helm repo index helm
+helm repo index charts
